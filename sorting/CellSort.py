5c18df9c6af4b2ac92d44fd7fef1593c65173a1f
luckycontrol <beansu1@naver.com>
2021-05-30 23:14:44 +0900

    shell sort

---
+++ sorting/CellSort.py
@@ -0,0 +1,37 @@
+def run():
+    arr = [2, 1, 5, 6, 4, 3, 9, 8]
+    size = 8
+    newArr = cellSort(arr, size)
+
+    print(newArr)
+
+def cellSort(arr, size):
+
+    _arr = arr
+
+    gap = size // 2
+    if gap != 0 and gap % 2 == 0: gap += 1
+
+    while gap > 0:
+        for i in range(0, gap):
+            _arr = insertionSort(_arr, i, size-1, gap)
+        
+        gap = gap // 2
+        if gap != 0 and gap % 2 == 0: gap += 1
+
+    return _arr
+
+def insertionSort(arr, first, last, gap):
+    _arr = arr
+    for i in range(first + gap, last, gap):
+        key = _arr[i]
+
+        for j in range(i - gap, first, -gap):
+            if arr[j] > key: arr[j + gap] = arr[j]
+            else: arr[j + gap] = key; break
+        
+    return _arr
+
+
+if __name__ == "__main__":
+    run()
