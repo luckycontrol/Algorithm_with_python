e79059a044d731e22cfd60bb86c0502e644b1d86
luckycontrol <beansu1@naver.com>
2021-07-07 20:05:41 +0900

    UnionFind

--- Graph/UnionFind.py
+++ Graph/UnionFind.py
@@ -1,15 +1,23 @@
-MAX_VERTICES = 5
-parent = [-1] * MAX_VERTICES
-num = [1] * MAX_VERTICES
-
 def run():
     pass
 
-def find(v):
-    pass
+class Union_Find:
+    def __init__(self, vertices):
+        self.parent = [-1] * vertices
+        self.num = [1] * vertices
 
-def union(x, y):
-    pass
+    def set_union(self, x, y):
+        if self.num[x] > self.num[y]:
+            self.parent[y] = x
+            self.num[x] += self.num[y]
+
+        else:
+            self.parent[x] = y
+            self.num[y] += self.num[x]
+
+    def set_find(self, vertex):
+        if self.parent[vertex] == -1: return vertex
+        if self.parent[vertex] != -1: return self.set_find(self.parent[vertex])
 
 if __name__ == "__main__":
     run()
